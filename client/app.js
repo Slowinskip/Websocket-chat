ebce6d3c7b361b76cfb7649030fe85acfcb52b19
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-11-17 19:15:52 +0100

    Add express server and fix bug with .innerHTML in app.js

--- client/app.js
+++ client/app.js
@@ -1,11 +1,10 @@
 
 const loginForm = document.getElementById('welcome-form');
 const messagesSection = document.getElementById('messages-section');
-const messagesList = document.getElementById('messages-section__list');
+const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
-
 
 let userName = "";
 
@@ -25,7 +24,7 @@ function login(event) {
 
 function sendMessage(event) {
     event.preventDefault();
-
+    console.log('SendMessage');
     if( messageContentInput.value == '') {
         alert('Message cannot be empty')
     } else {
@@ -38,20 +37,15 @@ function addMessage(author, content) {
     const message = document.createElement('li');
     message.classList.add('message');
     message.classList.add('message--received');
-
-    if ( author === userName ){
-        message.classList.add('message--self');
-    }
-
+    if(author === userName) message.classList.add('message--self');
     message.innerHTML = `
-        <h3 class="message__author">${userName === author ? 'You' : author }</h3>
-        <div class="message__content">
-            ${content}
-        </div>
+      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+      <div class="message__content">
+        ${content}
+      </div>
     `;
     messagesList.appendChild(message);
-}
-
+  }
 
 loginForm.addEventListener('submit', (e) => {
     login(e);
